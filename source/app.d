0ed4443790adc07620d81e79f65f2be0033a76be
Lubov <lubov@MacBook-Air-Lubov.local>
2020-02-25 06:56:57 +1000

    Added some functionality

--- source/app.d
+++ source/app.d
@@ -6,7 +6,13 @@ import licelformat;
 void main()
 {
     LicelFile f = new LicelFile("qqq");
+	LicelProfile lp = new LicelProfile(" 1 0 1 16380 1 0000 7.50 00355.o 0 0 00 000 12 000069 0.500 BT0");
+	EDatasetType dt=EDatasetType.dtAnalog;
 	//f.m_Profiles["1"] = new LicelProfile();
+	writeln(lp.isActive, ' ', lp.dsType, ' ', lp.laserPolstate, ' ', lp.scatPolStatus, ' ', lp.wavelength, ' ', lp.deviceID);
+	writeln(dt);
 	
-	//writeln(f.m_Profiles);
+	dt=cast(EDatasetType)(2);
+	writeln(dt);
+	
 }
--- source/licelformat.d
+++ source/licelformat.d
@@ -1,5 +1,10 @@
 module licelformat;
 import std.datetime.date;
+import std.array;
+import std.uni : isWhite;
+import std.conv;
+import std.stdio;
+
 
 enum EDatasetType {
 	dtAnalog=0,
@@ -16,12 +21,53 @@ enum EPolarizationStatus{
 	psLeftCircular		
 }
 
+enum EPolarizationScattStatus:char{
+	psNone='o',
+	psParallel='p',
+	psCross='s',
+	psRightCircular='r',
+	psLeftCircular='l'		
+}
+
 
 class LicelProfile {
 	
 	@property bool isActive() {return m_isActive;}
 	@property EDatasetType dsType(){return m_datsetType;}
 	@property bool isPhoton() {return m_datsetType==EDatasetType.dtPhoton;}
+	@property int nDataPoints(){return m_nDataPoints;}
+	@property EPolarizationStatus laserPolstate(){return m_laserPolstate;}
+	@property double wavelength(){return m_wavelength;}
+	@property EPolarizationScattStatus scatPolStatus(){return m_scatPolStatus;}
+	@property int nShots(){return m_nShots;}
+	@property double discrLevel(){return m_discrLevel;}
+	@property int deviceID(){return m_deviceID;}
+	
+	this(string dataline){
+		parseLine(dataline);
+	}
+	
+private:
+	void parseLine(string dataline){
+		auto items=dataline.split();
+		writeln(items);
+		m_isActive = cast(bool)(parse!int(items[0]));
+		m_datsetType = cast(EDatasetType)(parse!int(items[1]));
+		m_nlaser = parse!int(items[2]);
+		m_nDataPoints = parse!int(items[3]);
+		m_laserPolstate=cast(EPolarizationStatus)parse!int(items[4]);
+		m_highVoltage=parse!double(items[5]);
+		m_binWidth=parse!double(items[6]);
+		auto tmp=items[7].split(".");
+		m_wavelength=parse!double(tmp[0]);
+		m_scatPolStatus=cast(EPolarizationScattStatus)(tmp[1][0]);
+		m_binShift = parse!int(items[10]);
+		m_decBinShift = parse!int(items[11]);
+		m_adcBits = parse!int(items[12]);
+		m_nShots=parse!int(items[13]);
+		m_discrLevel = parse!double(items[14]);
+		m_deviceID = cast(int)(items[15][2]-'0');
+	}
 	
 private:
 	bool m_isActive;
@@ -30,8 +76,8 @@ private:
 	int m_nDataPoints;
 	EPolarizationStatus m_laserPolstate;
 	double m_highVoltage;
-	double m_binWidth, m_wavelengths;
-	EPolarizationStatus m_polStatus;
+	double m_binWidth, m_wavelength;
+	EPolarizationScattStatus m_scatPolStatus;
 	int m_binShift, m_decBinShift;
 	int m_adcBits;
 	int m_nShots;
